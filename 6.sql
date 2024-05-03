214306278a3ba47d428c681d3b8b5ed8b93cb5bc
Илья Волочинский ТКИ-442 <117316669+DikijOniRonin@users.noreply.github.com>
2024-05-03 20:32:43 +0300

    Исправлены работы 4, 5 и 6

--- 6.sql
+++ 6.sql
@@ -1,3 +1,3 @@
 -- Показать список продуктов из таблицы Production.Product с таким же цветом как у
 -- продукта «ML Road Frame–W – Yellow, 38», используя SELF JOIN.
-SELECT FROM "Production"."ProductInventory" ppi1 JOIN "Production"."ProductInventory" ppi2 ON
+SELECT * FROM "Production"."Product" pp1 JOIN "Production"."Product" AS pp2 ON pp1."Color"=pp2."Color" WHERE pp1."Name"='ML Road Frame–W – Yellow, 38'
