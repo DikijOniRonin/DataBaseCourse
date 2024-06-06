00b7eefbc1b3d4caf5cb1c65d276aa9575e0bfa1
Илья Волочинский ТКИ-442 <117316669+DikijOniRonin@users.noreply.github.com>
2024-06-06 17:50:53 +0300

    Исправление

--- 4.sql
+++ 4.sql
@@ -1,5 +1,10 @@
 -- 4
 -- Для каждого из продуктов выведите его название и цену, а также название и цену более дорогого продукта("Production"."Product")
-SELECT pp1."Name", pp1."ListPrice", pp2."Name" AS more_expensive_product_name,
-       pp2."ListPrice" AS more_expensive_product_price FROM "Production"."Product"
-           pp1 LEFT JOIN "Production"."Product" pp2 ON pp1."ListPrice" < pp2."ListPrice"
+SELECT pp1."Name", pp1."ListPrice",
+       (SELECT pp2."Name" FROM "Production"."Product" pp2
+        WHERE pp2."ListPrice" > pp1."ListPrice"
+        ORDER BY pp2."ListPrice" LIMIT 1) AS more_expensive_product_name,
+       (SELECT pp2."ListPrice" FROM "Production"."Product" pp2
+        WHERE pp2."ListPrice" > pp1."ListPrice"
+        ORDER BY pp2."ListPrice" LIMIT 1) AS more_expensive_product_price
+FROM "Production"."Product" pp1
