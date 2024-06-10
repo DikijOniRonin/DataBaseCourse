53ea227451594d532605233d84d0401423e57d04
Илья Волочинский ТКИ-442 <117316669+DikijOniRonin@users.noreply.github.com>
2024-06-10 15:02:04 +0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,4 +1,4 @@
 -- 2
 -- Ранжирование сотрудников по дате приема на работу в порядке от самого старого до самого нового сотрудника("HumanResources"."Employee").
 -- Примечание к ранжированию - ранг может начинаться с произвольного числа (например, 2 или 5), главное - ранг не должен уменьшаться
-select "BusinessEntityID","HireDate", ROW_NUMBER() OVER (ORDER BY "HireDate") as Rank from "HumanResources"."Employee"
+SELECT "BusinessEntityID", "HireDate", (SELECT COUNT(*) FROM "HumanResources"."Employee" e2 WHERE e2."HireDate" <= e1."HireDate") as Rank FROM  "HumanResources"."Employee" e1 ORDER BY  "HireDate";
